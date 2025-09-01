bbc22eb4f19c88bc1682662f7845e8236951a76b
Walentin <valoliinykov.work@gmail.com>
2025-09-01 18:17:14 +0300

    models

--- models/models.py
+++ models/models.py
@@ -1,20 +1,44 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import Boolean, DateTime, ForeignKey, String, Text, func
+from sqlalchemy import Boolean, DateTime, ForeignKey, String, Text, func, Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from settings import Base
+
+
+class RequestStatus(str, Enum):
+    NEW = "Нова"
+    IN_PROGRESS = "В обробці"
+    MESSAGE = "Повідомлення"
+    COMPLETED = "Завершено"
+    CANCELLED = "Скасовано"
 
 
 class RepairRequest(Base):
     __tablename__ = "repair_requests"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    photo_url: Mapped[str] = mapped_column(String(255), nullable=True)
+    status: Mapped[RequestStatus] = mapped_column(SQLEnum(RequestStatus), default=RequestStatus.NEW, nullable=False)
 
-    # TODO
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    admin_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)    
+    
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
 
-    admin_messages: Mapped[list["AdminMessage"]] = relationship(back_populates="repair_request")
-    service_records: Mapped["ServiceRecord"] = relationship(back_populates="repair_request")
+
+    user: Mapped["User"] = relationship(back_populates="repair_requests", foreign_keys=[user_id], lazy="selectin")
+    admin: Mapped["User"] = relationship(back_populates="assigned_repair_requests", foreign_keys=[admin_id], lazy="selectin")
+    
+    admin_messages: Mapped[list["AdminMessage"]] = relationship(back_populates="repair_request", lazy="selectin")
+    service_records: Mapped["ServiceRecord"] = relationship(back_populates="repair_request", lazy="selectin")
+
+    def __str__(self):
+        return f"<RepairRequest {self.id} - {self.status}>"
+
 
 
 # Модель Note
@@ -27,7 +51,7 @@ class User(Base):
     password: Mapped[str] = mapped_column(String(100), nullable=False)  # Зберігаємо хеш пароля
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    repair_requests: Mapped[list["RepairRequest"]] = relationship(back_populates="user")
+    repair_requests: Mapped[list["RepairRequest"]] = relationship(back_populates="user", lazy="selectin")
     assigned_repair_requests: Mapped[list["RepairRequest"]] = relationship(back_populates="admin")
     
     admin_messages: Mapped[list["AdminMessage"]] = relationship(back_populates="admin")
